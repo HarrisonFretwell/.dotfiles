8db9ece1ec9e4900c001c1845879db31177fd402
Harrison Fretwell <harrison.fretwell@razor.co.uk>
2022-11-23 10:48:05 +0000

    Revert "Async formatting"
    This reverts commit e823f62318579bbe3240a26602fbf33a70f89a17.

--- nvim/lua/harrison/packer.lua
+++ nvim/lua/harrison/packer.lua
@@ -57,8 +57,6 @@ return require("packer").startup(function(use)
 					filetypes = {
 						javascript = true, -- allow specific filetype
 						typescript = true, -- allow specific filetype
-						typescriptreact = true, -- allow specific filetype
-						tsx = true, -- allow specific filetype
 						["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
 					},
 				})
