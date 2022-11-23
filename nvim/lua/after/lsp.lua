2506c86d8c091f05c8eba289056d230d133bc67d
Harrison Fretwell <harrison.fretwell@razor.co.uk>
2022-11-23 11:03:54 +0000

    Disable tsserver formatting

--- nvim/lua/after/lsp.lua
+++ nvim/lua/after/lsp.lua
@@ -19,7 +19,11 @@ local function on_attach(_, bufnr) end
 
 -- TypeScript
 nvim_lsp.tsserver.setup({
-	on_attach = on_attach,
+	-- Disable formatting, done by null-ls instead
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	on_attach = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+	end,
 	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
 	cmd = { "typescript-language-server", "--stdio" },
 })
