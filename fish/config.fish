d816bab4318a1825dfcb104071fc0137ef2ee1b6
Harrison Fretwell <harrison.fretwell@razor.co.uk>
2022-12-17 11:33:58 +0000

    Add nvm to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set PATH /opt/homebrew/bin $PATH
+set PATH /.profile $PATH
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
 starship init fish | source
